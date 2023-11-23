f159eeccb0b092b1d2359d62613cb2c5e34a068b
Benedek Robert George <benedek_robert2001@yahoo.com>
2023-11-23 18:28:25 +0200

    refactor logic behind images

--- src/components/InputComponents/MusicDataNftForm.tsx
+++ src/components/InputComponents/MusicDataNftForm.tsx
@@ -3,8 +3,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { Button } from "../../libComponents/Button";
-import { ArrowUp, ArrowDown, DeleteIcon, Trash2 } from "lucide-react";
+import { ArrowUp, ArrowDown, DeleteIcon, Trash2, Edit2 } from "lucide-react";
 import { ACCEPTED_IMAGE_TYPES, MAX_FILE_SIZE } from "../../utils/constants";
+import songFallbackImage from "../../assets/img/audio-player-image.png";
 
 // validation schema
 const formSchema = z.object({
@@ -14,22 +15,22 @@ const formSchema = z.object({
   album: z.string().min(1, "Required field"),
   title: z.string().min(1, "Required field"),
   coverArt: z.string().min(1, "Required field"),
-
-  trackFile: z
-    .any()
-    .refine(
-      (file) => {
-        //console.log("SIZE", file);
-        return file[0] && file[0].size <= MAX_FILE_SIZE;
-      },
-      { message: `Max song size is 5MB.` }
-    )
-    .refine(
-      (file) => {
-        return file[0] && file[0].type === "audio/mpeg";
-      },
-      { message: "Only audio/mpeg formats are supported." } /// maybe add more
-    ),
+  trackFile: z.string().min(1, "Required field"),
+  // trackFile: z   ///TODO FIND A WAY TO ADD THE VALIDATION
+  //   .any()
+  //   .refine(
+  //     (file) => {
+  //       //console.log("SIZE", file);
+  //       return file[0] && file[0].size <= MAX_FILE_SIZE;
+  //     },
+  //     { message: `Max song size is 5MB.` }
+  //   )
+  //   .refine(
+  //     (file) => {
+  //       return file[0] && file[0].type === "audio/mpeg";
+  //     },
+  //     { message: "Only audio/mpeg formats are supported." } /// maybe add more
+  //   ),
   // coverArt: z
   //   .any()
   //   .refine(
@@ -56,6 +57,9 @@ type MusicDataNftFormProps = {
 /// the form for each song that is going to be uploaded
 export function MusicDataNftForm(props: MusicDataNftFormProps) {
   const [isSaved, setIsSaved] = useState(false);
+  const [wantToEditImage, setwantToEditImage] = useState(false);
+  const [wantToEditAudio, setwantToEditAudio] = useState(false);
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -68,33 +72,48 @@ export function MusicDataNftForm(props: MusicDataNftFormProps) {
       coverArt: "",
     },
   });
-  const [file, setFile] = useState("");
-  const [mp3File, setMp3File] = useState();
+
+  const [imageURL, setImageURL] = useState("");
+  const [audioURL, setAudioURL] = useState("");
   const [imageFile, setImageFile] = useState<File>();
-  const [audioFile, setAudioFile] = useState();
+  const [audioFile, setAudioFile] = useState<File>();
 
   const handleImageFileChange = (event: any) => {
     const file = event.target.files[0];
     setImageFile(file);
+    const imageURL = URL.createObjectURL(event.target.files[0]);
+    form.setValue("coverArt", imageURL);
+    setImageURL(imageURL);
+    setwantToEditImage(false);
   };
 
   const handleAudioFileChange = (event: any) => {
     const file = event.target.files[0];
     setAudioFile(file);
+    const audioURL = URL.createObjectURL(event.target.files[0]);
+    form.setValue("trackFile", audioURL);
+    setAudioURL(audioURL);
+    setwantToEditAudio(false);
   };
+
   // if we want to update, prepopulate the form
   useEffect(() => {
-    setIsSaved(false);
+    //setIsSaved(false);
     form.setValue("date", props.song["date"] ? props.song["date"] : "");
     form.setValue("category", props.song["category"] ? props.song["category"] : "");
     form.setValue("artist", props.song["artist"] ? props.song["artist"] : "");
     form.setValue("album", props.song["album"] ? props.song["album"] : "");
     form.setValue("title", props.song["title"] ? props.song["title"] : "");
-    form.setValue("trackFile", props.song["trackFile"] ? props.song["trackFile"] : {});
+    form.setValue("trackFile", props.song["trackFile"] ? props.song["trackFile"] : "");
+    form.setValue("coverArt", props.song["coverArt"] ? props.song["coverArt"] : "");
+
     //form.setValue("trackFile", props.song["trackFile"] ? props.song["trackFile"] : {});
     //console.log("ARTTT", props.index, props.song["coverArt"]);
     //setImageFile(props.song["coverArt"] ? props.song["coverArt"] : null);
-    if (props.song["coverArt"]) setFile(props.song["coverArt"]);
+    if (props.song["coverArt"]) setImageURL(props.song["coverArt"]);
+    if (props.song["trackFile"]) setAudioURL(props.song["trackFile"]);
+    setImageFile(undefined);
+    setAudioFile(undefined);
   }, [props.song]);
 
   function onSubmit(values: z.infer<typeof formSchema>) {
@@ -108,8 +127,9 @@ export function MusicDataNftForm(props: MusicDataNftFormProps) {
     if (props.index == 1) return;
     props.swapFunction(props.index - 1 + 1, props.index - 1);
   }
+
   function handleMoveDown() {
-    props.swapFunction(Number(props.index), Number(props.index) + 1); // -1 solves the problem for now
+    props.swapFunction(Number(props.index), Number(props.index) + 1); // -1 transform string in number, solves the problem for now
   }
 
   function deleteSong() {
@@ -117,7 +137,7 @@ export function MusicDataNftForm(props: MusicDataNftFormProps) {
   }
 
   return (
-    <div className="w-[80%] z-2 p-4 flex flex-col bg-gradient-to-b from-sky-500/20 via-[#300171]/20 to-black/20 rounded-3xl shadow-xl hover:shadow-sky-500/50 max-w mx-auto">
+    <div className="  z-2 p-4 flex flex-col bg-gradient-to-b from-sky-500/20 via-[#300171]/20 to-black/20 rounded-3xl shadow-xl hover:shadow-sky-500/50 max-w mx-auto">
       <div className="relative">
         <div className="absolute top-0 right-0">
           <div className="flex flex-col justify-between">
@@ -130,7 +150,7 @@ export function MusicDataNftForm(props: MusicDataNftFormProps) {
           </div>
         </div>
       </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-row space-y-4 gap-4 ">
+      <form onChange={() => setIsSaved(false)} onSubmit={form.handleSubmit(onSubmit)} className="flex flex-row space-y-4 gap-4 ">
         <div>
           <div>
             <label className="block text-gray-700">Date</label>
@@ -170,55 +190,63 @@ export function MusicDataNftForm(props: MusicDataNftFormProps) {
           </div>
         </div>
         <div className="gap-4 flex-col flex items-center justify-center ">
-          <div className="justify-center">
-            <img className="justify-center allign-center w-32 h-32 border border-white" src={file} alt={"Cover Image"}></img>
+          <img
+            className="mx-auto flex justify-center allign-center w-32 h-32 border border-white"
+            src={imageURL ? imageURL : songFallbackImage}
+            alt={"Cover Image"}></img>
+          <div className="">
             <label className=" block text-gray-700">Cover Art Image</label>
-            {imageFile || file ? (
-              <p> we got the image</p>
+            {(imageFile || imageURL) && !wantToEditImage ? (
+              <div className="flex w-full ">
+                <p className="flex justify-center allign-center"> {imageFile ? imageFile.name : "Image.img or nothing ?"}</p>
+                <Button className="scale-75 ml-auto hover:shadow-inner hover:shadow-sky-400 " onClick={() => setwantToEditImage(true)}>
+                  <Edit2 scale={0.5}></Edit2>
+                </Button>
+              </div>
             ) : (
               <input
                 type="file"
                 accept="image/*"
-                // disabled={!!file}
                 className="w-full p-2 border border-gray-300 rounded"
                 onChange={(e) => {
-                  // console.log("COVER ART ", e.target.files);
-
-                  if (e.target.files) {
-                    handleImageFileChange(e);
-                    const imageURL = URL.createObjectURL(e.target.files[0]);
-                    form.setValue("coverArt", imageURL);
-                    setFile(imageURL);
-                  }
+                  if (e.target.files) handleImageFileChange(e);
                 }}
               />
             )}
-            {/*   {form.formState.errors.coverArt && <p className="text-red-500">{form.formState.errors.coverArt.message?.toString()}</p>} */}
+            {form.formState.errors.coverArt && <p className="text-red-500">{form.formState.errors.coverArt.message?.toString()}</p>}
           </div>
           <div>
             <label className="block text-gray-700">Track File (MP3)</label>
-            <input
-              type="file"
-              accept=".mp3"
-              disabled={mp3File}
-              className="w-full p-2 border border-gray-300 rounded"
-              {...form.register("trackFile")}
-              onChange={(e) => {
-                //setMp3File(true);
-                handleAudioFileChange(e);
 
-                //form.setValue("trackFile", e.target.files ? e.target.files[0] : "asd");
-              }}
-            />
+            {(audioFile || audioURL) && !wantToEditAudio ? (
+              <div className="flex justify-center flex-col w-full ">
+                <div className="flex flex-row justify-center">
+                  <audio className="scale-75" controls>
+                    <source src={audioURL} type="audio/mp3" />
+                    Your browser does not support the audio tag.
+                  </audio>
+                  {/* <p className="flex justify-center allign-center"> {audioFile?.name}</p> */}
+                  <Button className="scale-75  ml-auto  hover:shadow-inner hover:shadow-sky-400 " onClick={() => setwantToEditAudio(true)}>
+                    <Edit2 scale={0.5}></Edit2>
+                  </Button>
+                </div>
+              </div>
+            ) : (
+              <input type="file" accept=".mp3" className="w-full p-2 border border-gray-300 rounded" onChange={(e) => handleAudioFileChange(e)} />
+            )}
             {form.formState.errors.trackFile && <p className="text-red-500">{form.formState.errors.trackFile.message?.toString()}</p>}
           </div>
-          <button type="submit" className="hover:shadow-inner hover:shadow-sky-400   text-white p-2 rounded  ">
-            Save
-          </button>
-
-          <Button onClick={deleteSong} className="ml-auto hover:shadow-inner hover:shadow-red-400">
-            <Trash2 />
-          </Button>
+          {/* onClick={() => onSubmit(form.getValues())}  */}
+          <div className="w-full flex flex-row ">
+            <div className="w-full flex justify-center">
+              <button type="submit" className=" self-center hover:shadow-inner hover:shadow-sky-400   text-white p-2 rounded  ">
+                Save
+              </button>
+            </div>
+            <Button onClick={deleteSong} className="ml-auto flex justify-end self-end hover:shadow-inner hover:shadow-red-400">
+              <Trash2 />
+            </Button>
+          </div>
           {isSaved && <p className="text-green-400"> saved</p>}
         </div>
       </form>
--- src/pages/Upload/Upload.tsx
+++ src/pages/Upload/Upload.tsx
@@ -5,7 +5,6 @@ import { Button } from "../../libComponents/Button";
 import axios from "axios";
 import { useGetLoginInfo } from "@multiversx/sdk-dapp/hooks";
 import { API_URL } from "../../utils/constants";
-import { setDappConfig } from "@multiversx/sdk-dapp/reduxStore/slices";
 
 type SongData = {
   date: string;
@@ -13,19 +12,20 @@ type SongData = {
   artist: string;
   album: string;
   title: string;
-  trackFile: any;
-  coverArt: any;
+  trackFile: string;
+  coverArt: string;
 };
 type FilePair = {
+  idx: number;
   image: File;
   audio: File;
 };
+
 export const UploadData: React.FC = () => {
   const location = useLocation();
   const { action, type, template, storage, descentralized } = location.state;
   const [songsData, setSongsData] = useState<Record<number, SongData>>({});
-  // const [filePairs, setFilePairs] = useState([{image : File,audio : File}]);
-  const [filePairs, setFilePairs] = useState<FilePair[]>([]);
+  const [filePairs, setFilePairs] = useState<Record<number, FilePair>>({});
 
   console.log("SONGS DATA", songsData);
   const [numberOfSongs, setNumberOfSongs] = useState(1);
@@ -43,9 +43,9 @@ export const UploadData: React.FC = () => {
     stream: "no",
   });
   const theToken =
-    "ZXJkMXZ5ZWp2NTJlNDNmeHE5NmNzY2h5eWo5ZzU3cW45a2d0eHJoa2c5MmV5aGZ1NWEwMjJwbHF0ZHh2ZG0.YUhSMGNITTZMeTkxZEdsc2N5NXRkV3gwYVhabGNuTjRMbU52YlEuYjQzYzYxMTY4NDZjODU4MDIwMmY4Y2UxMTY2NWUxMmQ4YWE1ZWZmYmRiN2JjOTkzNTBmOGU4NTU3ZjFjOGIxMC43MjAwLmV5SjBhVzFsYzNSaGJYQWlPakUzTURBMk9UQXdPRGw5.63cedef0f407493e85e7fb20214c65a7424b98ee87df0a9e8fab435586ea11741ba82dbfff5cf95654957c697612fa8f436c81e283fde9704c208b1c8bc3330e";
-  const apiUrlGet = `${API_URL}/files`;
-  const apiUrl = `${API_URL}/upload`; //refactor this as env file or const
+    "ZXJkMXZ5ZWp2NTJlNDNmeHE5NmNzY2h5eWo5ZzU3cW45a2d0eHJoa2c5MmV5aGZ1NWEwMjJwbHF0ZHh2ZG0.YUhSMGNITTZMeTkxZEdsc2N5NXRkV3gwYVhabGNuTjRMbU52YlEuNDIxMmFmOTg1NDI5ZjllMmE3YWZiNzk0YzFmYTU4ZGFkMDIyNWFkNDczODc4MzI0MGM2ZTg2MDIyNDgzMzJkOC43MjAwLmV5SjBhVzFsYzNSaGJYQWlPakUzTURBM05URXdORGg5.67488297b3064378019343f20a41a593072b6acbd8c35a0c19916c7bf8de51eb1d2172bc76ffffd694dd7269dab215930404c0a4fe83b0d72a75aa66fd3a360f";
+  //const apiUrlGet = `${API_URL}/files`;
+  const apiUrlPost = `${API_URL}/upload`; //refactor this as env file or const
 
   useEffect(() => {
     console.log("EFFECT", songsData);
@@ -57,24 +57,12 @@ export const UploadData: React.FC = () => {
 
     //iterating over the songsData and for each object add its image and song to the formData
     Object.values(songsData).forEach((songData, idx) => {
-      const appendFileToFormData = (fileArray: any, fileNamePrefix: any) => {
-        if (fileArray && fileArray[0] instanceof File) {
-          const file = fileArray[0];
-          formData.append("files", file, file.name || `${fileNamePrefix}_${idx}`);
-        }
-      };
-      // let blob = await fetch(songData.coverArt).then((r) => r.blob());
-      // console.log(blob);
-      console.log("THE THING ", filePairs[idx].image);
-      console.log("THE TITLTE", songData.title, songData.coverArt);
-      formData.append("files", filePairs[idx].image, "image." + songData.title);
-      formData.append("files", filePairs[idx].audio, "audio." + songData.title);
-
-      // appendFileToFormData(songData.coverArt, "coverArt");
-      //appendFileToFormData(songData.trackFile, "trackFile");
+      // todo must change the way of storing, its not ok only by title
+      formData.append("files", filePairs[idx + 1].image, "image." + songData.title); ///   + "-" + filePairs[idx+1].image.name);
+      formData.append("files", filePairs[idx + 1].audio, "audio." + songData.title); //+ "-" + filePairs[idx+1].audio.name);
     });
     try {
-      const response = await axios.post(apiUrl, formData, {
+      const response = await axios.post(apiUrlPost, formData, {
         headers: {
           "authorization": `Bearer ${theToken}`,
           "Content-Type": "multipart/form-data",
@@ -89,41 +77,13 @@ export const UploadData: React.FC = () => {
   // get all songs data into the right format for manifest file
   async function transformSongsData() {
     setIsUploadingSongs(true);
-    console.log("TRANSFORM SONGS DATA");
-
-    //const formData = new FormData();
-
-    // const transformedData = Object.keys(songsData).map((idx: any) => {
-    //   const songData = songsData[parseInt(idx)];
-    //   //const imageBlob = new Blob([songData.coverArt[0]], { type: songData.coverArt[0].type });
-
-    //   // add for each song object the image file and song
-    //   formData.append("files", songData.coverArt[0], songData.coverArt[0]?.name || "custom_filename");
-    //   formData.append("files", songData.trackFile[0], songData.trackFile[0]?.name || "custom_filename");
-
-    //   return {
-    //     idx: parseInt(idx),
-    //     date: new Date(songData.date).toISOString(),
-    //     category: songData.category,
-    //     artist: songData.artist,
-    //     album: songData.album,
-    //     // file: "https://dataassets.valhallala.com/file_storage/hack1mus.mp3",
-    //     cover_art_url: songData.coverArt,
-    //     title: songData.title,
-    //   };
-    // });
     try {
       const responseDataCIDs = await uploadSongsAndImagesFiles();
       console.log("THE RESPONSE IS : ", responseDataCIDs);
 
       if (!responseDataCIDs) throw new Error("Upload songs did not work correctly");
-      console.log(songsData);
       // Iterate through the second list and find the matching cidv1
       const transformedData = Object.values(songsData).map((songObj, index) => {
-        // Find the object in the first list with the same fileName
-        // console.log("songObj.coverArt[0]?.name", songObj.coverArt[0]?.name);
-        // console.log("songObj.trach[0]?.name", songObj.trackFile[0]?.name);
-
         const matchingObjImage = responseDataCIDs.find((uploadedFileObj: any) => uploadedFileObj.fileName === `image.${songObj.title}`);
         const matchingObjSong = responseDataCIDs.find((uploadedFileObj: any) => uploadedFileObj.fileName === `audio.${songObj.title}`); ///songObj.trackFile[0]?.name);
         console.log("matchimg", matchingObjImage);
@@ -142,7 +102,7 @@ export const UploadData: React.FC = () => {
           title: songObj?.title,
         };
       });
-      console.log("DATA LIST ", transformedData);
+      console.log("Transformed DATA LIST ", transformedData);
 
       setIsUploadingSongs(false);
       return transformedData;
@@ -153,14 +113,9 @@ export const UploadData: React.FC = () => {
   }
 
   const generateManifestFile = async () => {
-    ///TODO HERE UPLOAD THE FILES
-    // const coverLink = async upload_image()
-    // const songURL = upload_song();
-    /// maybe add a remove button ??
-    console.log("should start transformData");
     const data = await transformSongsData();
     setIsUploadingManifest(true);
-    console.log("Transofemed for manifest DATA", data);
+
     const manifest = {
       "data_stream": {
         "name": formData.name,
@@ -175,22 +130,22 @@ export const UploadData: React.FC = () => {
       "data": data,
     };
     console.log("Manifest : ", manifest);
-    console.log("TOJEN", tokenLogin?.nativeAuthToken);
+    // console.log("TOJEN", tokenLogin?.nativeAuthToken);
 
     ///GETTER
 
     const formDataFormat = new FormData();
     formDataFormat.append("files", new Blob([JSON.stringify(manifest)], { type: "application/json" }), formData.name + "-" + formData.creator);
-    console.log(formDataFormat);
+
     try {
-      const response = await axios.post(apiUrl, formDataFormat, {
+      const response = await axios.post(apiUrlPost, formDataFormat, {
         headers: {
           "authorization": `Bearer ${theToken}`,
           "Content-Type": "multipart/form-data",
         },
       });
 
-      console.log("Response:", response.data);
+      console.log("Response upload manifest:", response.data);
       const ipfs: any = "ipfs://" + response.data[0].cidv1;
       if (response.data[0]) setManifestCid(ipfs);
       else {
@@ -217,39 +172,42 @@ export const UploadData: React.FC = () => {
   };
 
   function swapSongs(first: number, second: number) {
-    // console.log("swap SONG ", first, second);
-    // console.log("FISRT", songsData[first]);
-    // console.log("Second", songsData[second]);
     if (first < 1 || second > numberOfSongs) {
       return;
     }
 
     if (second === -1) {
-      // means we want to delete first song
+      // means we want to delete song with index first
       for (var i = first; i < numberOfSongs - 1; ++i) {
         songsData[i] = songsData[i + 1];
+        filePairs[i] = filePairs[i + 1];
       }
       delete songsData[numberOfSongs - 1];
+      delete filePairs[numberOfSongs - 1];
       setNumberOfSongs((prev) => prev - 1);
       return;
     }
 
-    console.log("SongsData before swap: ", songsData);
+    //console.log("SongsData before swap: ", songsData);
     const storeSong = songsData[second];
-    // console.log("Store song var", storeSong);
     var songsDataVar = songsData;
-    // console.log("ALL songs var ", songsDataVar);
-
     songsDataVar[second] = songsDataVar[first];
     songsDataVar[first] = storeSong;
-    // console.log("SWAPED SONG DATA", songsDataVar);
+
+    const storeFile = filePairs[second];
+    var storeFilesVar = filePairs;
+    storeFilesVar[second] = storeFilesVar[first];
+    storeFilesVar[first] = storeFile;
+
     console.log("after swap", songsDataVar);
     setSongsData(songsDataVar);
+    setFilePairs(storeFilesVar);
   }
 
   const handleFilesSelected = (index: number, formInputs: any, image: File, audio: File) => {
     console.log("IN PARENT :", index, formInputs, image, audio);
-    setFilePairs((prevFilePairs) => [...prevFilePairs, { image: image, "audio": audio }]);
+    if (image && audio) setFilePairs(Object.assign({}, filePairs, { [index]: { image: image, audio: audio } }));
+
     setSongsData((prev) => Object.assign({}, prev, { [index]: formInputs }));
     console.log("SIR de files", filePairs);
   };
@@ -257,7 +215,6 @@ export const UploadData: React.FC = () => {
   return (
     <div className="p-4 flex flex-col">
       <b className=" py-2 text-xl  font-medium dark:text-purple-700"> Let’s update your data! Here is what you wanted to do... </b>
-      {/* <b className="p-2 text-lg  font-medium  dark:text-blue-400">{props.description} </b> */}
       <div className="flex flex-row gap-4 mb-4">
         {action && (
           <span className="w-32 border-2 text-bold border-blue-400 bg-blue-900 text-blue-400 text-center text-sm font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
