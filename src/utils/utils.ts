514b726e19c7a1312835e49606612930484f9970
Bucur David <dbucur22@gmail.com>
2023-12-08 12:37:12 +0200

    refactor: keep the extension of files && add prettier configuration for consistency
    Signed-off-by: Bucur David <dbucur22@gmail.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -4,3 +4,9 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function generateRandomString() {
+  const timestampInSeconds = Math.floor(new Date().getTime() / 1000);
+  const randomNum = Math.floor(Math.random() * 1000000);
+  return `${timestampInSeconds}-${randomNum}-`;
+}
