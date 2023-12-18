d5766b6e252353d98058b435649fe4116f62433e
Benedek Robert George <benedek_robert2001@yahoo.com>
2023-12-18 13:24:31 +0200

    typo: deploy test

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ export const Home: React.FC = () => {
   return (
     <div className="flex flex-row justify-center items-center w-full tracking-wide ">
       <span className="text-4xl leading-relaxed font-medium">
-       Unified centralized and decentralized storage solution for enterprises and individuals.
+        Unified centralized and decentralized storage solution for enterprises and individuals- deploy test.
       </span>
     </div>
   );
