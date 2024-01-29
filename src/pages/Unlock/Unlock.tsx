80f5270e100361306e8b5bcff173f4564c9c0fb0
Mark Paul <paul.mark@gmail.com>
2024-01-29 13:53:08 +1100

    wallet popup done

--- src/pages/Unlock/Unlock.tsx
+++ src/pages/Unlock/Unlock.tsx
@@ -30,27 +30,35 @@ export const Unlock: React.FC = () => {
   };
 
   return (
-    <div className="flex items-center justify-center w-full h-screen bg-background z-[-2]">
+    <div className="flex items-center justify-center w-full bg-background">
       <img src={zImageHalf} className="z-[-1] absolute right-0 max-w-[30rem] w-[60%] h-screen"></img>
-      <div className="w-[30%] relative bg-muted rounded-[20px] border border-accent/25 border-opacity-5 p-4">
+      <div className="w-[38%] relative bg-muted rounded-[20px] border border-accent/25 border-opacity-5 p-4">
         <div className="flex flex-col w-full gap-2">
           <div className="w-[100%] bg-gradient-to-r from-muted via-accent/50 to-muted pb-[1px] -z-1">
-            <div className="w-full bg-muted flex justify-center  text-accent text-2xl font-medium p-3 z-3">Create or Connect a Digital Wallet</div>
+            <div className="w-full bg-muted flex justify-center text-accent text-2xl font-medium">Create or Connect a Digital Wallet</div>
+            <div className="w-full bg-muted flex justify-center text-xs font-medium p-3 z-3">
+              A digital wallet is what ensures that you have sovereignty over your zStorage data bunker.
+            </div>
           </div>
+
+          <p className="text-xs flex justify-center mt-3">Use your Google Account to generate your sovereign digital wallet.</p>
           <WebWalletLoginButton
             className={buttonStyles}
             loginButtonText={"Google (via xAlias)"}
             customWalletAddress="https://devnet.xalias.com"
             {...commonProps}></WebWalletLoginButton>
-          <ExtensionLoginButton className={buttonStyles} {...commonProps} loginButtonText={"Extension"} />
+
+          <div className="text-xs flex justify-center mt-5">... or use any wallet that's compatible with the MultiversX Blockchain</div>
+
+          <ExtensionLoginButton className={buttonStyles} loginButtonText={"DeFi Browser Wallet"} {...commonProps} />
           <WalletConnectLoginButton
             className={buttonStyles}
+            loginButtonText="xPortal Mobile Wallet"
             {...commonProps}
-            loginButtonText="xPortal App"
             {...(walletConnectV2ProjectId ? { isWalletConnectV2: true } : {})}
           />
-          <WebWalletLoginButton className={buttonStyles} {...commonProps} loginButtonText={"Web wallet"} />
-          <LedgerLoginButton className={buttonStyles} loginButtonText="Ledger" {...commonProps} />
+          <LedgerLoginButton className={buttonStyles} {...commonProps} loginButtonText="Ledger Hardware Wallet" />
+          <WebWalletLoginButton className={buttonStyles} {...commonProps} loginButtonText={"Web Wallet"} />
         </div>
       </div>
     </div>
