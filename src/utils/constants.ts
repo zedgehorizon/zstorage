4d0e21cee1937cc9c8c57b1e62dabe5971b08066
Benedek Robert George <benedek_robert2001@yahoo.com>
2024-02-01 22:50:16 +0200

    fix: change env backend to const

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -6,3 +6,4 @@ export const MAX_FILE_SIZE = 5000000;
 export const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
 export const theToken =
   "ZXJkMXZ5ZWp2NTJlNDNmeHE5NmNzY2h5eWo5ZzU3cW45a2d0eHJoa2c5MmV5aGZ1NWEwMjJwbHF0ZHh2ZG0.YUhSMGNITTZMeTkxZEdsc2N5NXRkV3gwYVhabGNuTjRMbU52YlEuODI4MTYyZmFmMmQ4ODExZTFmZWJhYjE1MzIxMzU2NTAyM2QyYWY0YjQ5M2ZmYmQ1NTRhZWQ5OGYxMzlmMTc3ZS43MjAwLmV5SjBhVzFsYzNSaGJYQWlPakUzTURZNE1UVTBOako5.453682cdcb91ff330d219d51a4a8f23fec42822efc1ecba6f566e910a23889584cce3be12f7e53eb3ecaacc424206563ab12439079dac81a39abdb79ae6eb003";
+export const VITE_ENV_BACKEND_API = "https://zsuite-backend.up.railway.app";
